22a61701a261241b1f2408e3569ad9df2b607ce4
Elba Chimilio <elbachimilio@gmail.com>
2019-11-14 18:40:44 -0500

    Adding resources to footer

--- client/src/components/general/Footer.jsx
+++ client/src/components/general/Footer.jsx
@@ -5,23 +5,27 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        Website designed by @elba
-        <a href="https://github.com/elba3184" target="_blank"><img src="../images/github-logo.svg" /></a>
-        <a href="https://www.linkedin.com/in/elbachimilio/" target="_blank"><img src="../images/linkedin-logo.svg" /></a>
-        <a href="mailto:emchimilio@gmail.com" target="_blank"><img src="../images/gmail-logo.svg" /></a>
-        Resources:
-          <p className="resources">GitHub Icon made by
+        <div className="footer-icons">
+          <a href="https://github.com/elba3184" target="_blank"><img src="../images/github-logo.svg" className="github" /></a>
+          <a href="https://www.linkedin.com/in/elbachimilio/" target="_blank"><img src="../images/linkedin-logo.svg" className="linkedin" /></a>
+          <a href="mailto:emchimilio@gmail.com" target="_blank"><img src="../images/gmail-logo.svg" className="gmail" /></a>
+        </div>
+        <div className="footer-resources">
+          Website designed by @elbachimilio
+         Resources:
+          <p className="resources"> GitHub Icon made by
           <a href="https://www.flaticon.com/authors/dave-gandy" target="_blank">Dave Gandy</a>
-          from www.flaticon.com
+            from www.flaticon.com |
           </p>
-        <p className="resources">Linkedin Icon made by
+          <p className="resources"> Linkedin Icon made by
           <a href="https://www.flaticon.com/authors/freepik" target="_blank">Freepik</a>
-          from www.flaticon.com
+            from www.flaticon.com |
           </p>
-        <p className="resources">Gmail Icon made by
+          <p className="resources"> Gmail Icon made by
           <a href="https://www.flaticon.com/authors/google" target="_blank">Google</a>
-          from www.flaticon.com
+            from www.flaticon.com
           </p>
+        </div>
       </div>
     )
   }
